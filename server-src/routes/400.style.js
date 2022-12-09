8a59e18f635c0cf7e6541c52f7767379b370afaa
Ed Armstrong <ed@frar.ca>
2022-12-09 12:15:11 -0500

    clean up 400.style.js

--- server-src/routes/400.style.js
+++ server-src/routes/400.style.js
@@ -2,59 +2,73 @@ import Express from "express";
 import Path from "path";
 import sass from "sass";
 import FS from "fs";
-import { mkdirif, seekfiles } from "@thaerious/utility";
+import { mkdirif } from "@thaerious/utility";
 import logger from "../setupLogger.js";
 
 const router = Express.Router();
+const SCSS_SRC = process.env.SCSS_SRC || "www/views";
+const CSS_COMPILED_DIR = process.env.CSS_COMPILED_DIR || "www/compiled";
+const NODE_MODULES = process.env.NODE_MODULES || "node_modules";
+const STYLE_EXT = process.env.STYLE_EXT || ".scss";
 
-const SRC_DIR = "www/views";
-const COMPILED_DIR = "www/compiled";
-
-// Compile scss files on demand.
+/**
+ * Render scss files on demand.
+ * If the .scss file is older than the .css file, it will get rendered.
+ * It will search the SCSS_SRC directory for a path matching the url.
+ * The resulting .css file will be placed in the CSS_COMPILED_DIR.
+ * Include files can be placed in the SCSS_SRC or NODE_MODULES directories.
+ * This middleware will create the destination directory if it doesn't exist.
+ */
 router.use("*.css", (req, res, next) => {
-    logger.verbose(`Compile Stylesheet`);
+    logger.verbose(`Render Stylesheet`);
     logger.verbose(`  \\_ URL ${req.originalUrl}`);
 
-    const destFilename = req.originalUrl;
-    const destParsed = Path.parse(destFilename);
-    const srcFilename = Path.join(SRC_DIR, destParsed.dir, destParsed.name + ".scss");
-    const finalDestination = Path.join(COMPILED_DIR, destFilename)
+    const destParsed = Path.parse(req.originalUrl);
+    const srcFullPath = Path.join(SCSS_SRC, destParsed.dir, destParsed.name + STYLE_EXT);
+    const destFullPath = Path.join(CSS_COMPILED_DIR, req.originalUrl)
 
-    if (!FS.existsSync(srcFilename)) {
-        logger.verbose(`  \\_ no source: ${srcFilename}`);
+    // Reject if there is no matching .scss file.
+    if (!FS.existsSync(srcFullPath)) {
+        logger.verbose(`  \\_ no source: ${srcFullPath}`);
         return next();
     }
 
-    if (FS.existsSync(finalDestination)) {
-        const destStat = FS.statSync(finalDestination);
-        const srcStat = FS.statSync(srcFilename);
+    // Reject if the .css file is newer than the .scss file.
+    if (FS.existsSync(destFullPath)) {
+        const srcStat = FS.statSync(srcFullPath);
+        const destStat = FS.statSync(destFullPath);        
         if (srcStat.mtime < destStat.mtime) {
-            logger.verbose(`  \\_ destination file newer: ${srcFilename}`);
+            logger.verbose(`  \\_ destination file newer: ${srcFullPath}`);
             return next();
         }
     }
 
-    logger.verbose(`  \\_ source ${srcFilename}`);
-    logger.verbose(`  \\_ generating: ${finalDestination}`);
-    compileSASS(srcFilename, finalDestination);
+    logger.verbose(`  \\_ source ${srcFullPath}`);
+    logger.verbose(`  \\_ generating: ${destFullPath}`);
+    compileSASS(srcFullPath, destFullPath);
     return next();
 });
 
-function compileSASS(srcFilename, destFilename) {
+/**
+ * Compile source .css to .scss with source map file.
+ * @param {#} srcFullPath full path of .css source file
+ * @param {*} destFullPath full path of .scss source file
+ */
+function compileSASS(srcFullPath, destFullPath) {
     const sassOptions = {
         loadPaths: [
-            Path.resolve("www/views"),
-            Path.resolve("node_modules"),
+            Path.resolve(SCSS_SRC),
+            Path.resolve(NODE_MODULES),
         ],
         sourceMap: true
     };
 
-    const result = sass.compile(srcFilename, sassOptions);
+    const result = sass.compile(srcFullPath, sassOptions);
 
     if (result) {
-        const destPath = mkdirif(destFilename);
-        FS.writeFileSync(destFilename, result.css);
-        FS.writeFileSync(destFilename + ".map", JSON.stringify(result.sourceMap));
+        mkdirif(destFullPath);
+        FS.writeFileSync(destFullPath, result.css);
+        FS.writeFileSync(destFullPath + ".map", JSON.stringify(result.sourceMap));
     }
 }
 
