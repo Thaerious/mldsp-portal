f29559ad35be9ceebbfecf71d939edb4a943673c
Ed Armstrong <ed@frar.ca>
2023-01-19 12:34:26 -0500

    added install node script

---
+++ tools/install_node.sh
@@ -0,0 +1,16 @@
+VERSION="${1:-18.0.0}"
+
+if [ ! -d "/opt/node/$VERSION" ]
+then
+    mkdir /opt/node
+    cd /opt/node
+    wget https://nodejs.org/dist/v$VERSION/node-v$VERSION-linux-x64.tar.xz
+    tar -xvf node-v$VERSION-linux-x64.tar.xz
+    rm node-v$VERSION-linux-x64.tar.xz
+    mv node-v$VERSION-linux-x64 /opt/node/$VERSION
+fi 
+
+ln -sf /opt/node/$VERSION/bin/node /usr/local/bin/nodejs
+ln -sf /opt/node/$VERSION/bin/node /usr/local/bin/node
+ln -sf /opt/node/$VERSION/bin/npm /usr/local/bin/npm
+ln -sf /opt/node/$VERSION/bin/npx /usr/local/bin/npx
